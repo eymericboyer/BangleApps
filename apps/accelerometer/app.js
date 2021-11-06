e621f1da6a0c7cd02f87539690a6601cb26d8b4e
eymericboyer <91205812+eymericboyer@users.noreply.github.com>
2021-11-06 13:28:44 +0100

    Update app.js

--- apps/accelerometer/app.js
+++ apps/accelerometer/app.js
@@ -1,79 +1,176 @@
-var durationInSeconds = 600.0;
 var counterInterval;
-var hertz = 25; //max 25 
+var hertz = 20; //max 20 
 var counter = 0;
-var fileName = "accelTestInLab00.csv";
+var fileNumber = 0;
+var username = "testerEB";
+var hours = 0;
+var minutes = 0;
+var seconds = 10;
+var paused = false;
+var initialDuration;
 
+//Allow to create file name
+function getFileName(username, fileNumber) {
+  return username + "_accelData_" + fileNumber + ".csv";
+}
+
+var fileName;// = getFileName(username, fileNumber);
 
 //Create file in append mode
-var file = require("Storage").open(fileName,"a");
+var file;// = require("Storage").open(fileName,"a");
+
+function durationInSeconds(hours, minutes, seconds){
+   return hours*3600 + minutes * 60 + seconds;
+}
+
+//Display Menu
+function showMenu() {
+  var menu = {
+    "" : { title : "User "+username },
+    "Exit" : function() {
+      load();
+    },
+    "File Number" : {
+      value : fileNumber,
+      min : 0,
+      max : 99,
+      onchange : v => { fileNumber=v; }
+    },
+    "Minutes" : {
+      value : minutes,
+      min : 0,
+      max : 20,
+      onchange : v => { minutes = v;}
+    },
+    "Seconds" : {
+      value : seconds,
+      min : 0,
+      max : 59,
+      onchange : v => { seconds = v;}
+    },
+    "Hz (max 20)" : {
+      value : hertz,
+      min : 0,
+      max : 20,
+      onchange : v => { hertz = v;}
+    },
+    "Start" : function() {
+      E.showMenu();
+      startRecording();
+    },
+  };
+
+  E.showMenu(menu);
+}
+
+function getNbAsString(nb) {
+  var toReturn = nb.toString();
+  if(nb<9){
+    toReturn = "0"+toReturn;
+  }
+  return toReturn;
+}
 
 function outOfTime() {
   if(counterInterval) return;
-
-  E.showMessage("Took " + counter + " measurements", durationInSeconds + "s elapsed");
+  
+  E.showMessage("Took " + counter + " measurements", initialDuration + "s elapsed");
   Bangle.buzz();
   Bangle.beep(200, 4000)
     .then(() => new Promise(resolve => setTimeout(resolve, 200)))
     .then(() => Bangle.beep(200, 3000));
-
+  
   //repeat after 5s
   setTimeout(outOfTime, 5000);
+ 
+}
 
+function createFirstLine() {
+  var firstLine = [
+    "Time",
+    "X",
+    "Y",
+    "Z",
+    "Diff",
+    "Magnitude"
+  ];
+  file.write(firstLine.join(",")+"\n");
 }
 
 function addLine(time, x,y,z,diff, mag) {
   var csv = [
-    time,
+    time/1000,
     x,
     y,
     z,
     diff,
     mag
   ];
-
+  
   file.write(csv.join(",")+"\n");
 }
 
+function pauseUnpause() {
+  paused = !paused;
+}
+
+function printDurationLeft(duration){
+  var sec = 0;
+  var min = 0;
+  var hr = 0;
+  if(duration < 3600){
+     if(duration < 60) {
+       return duration + "s";
+     } else {
+       sec = duration%60;
+       min = (duration - seconds)/60;
+       return min + "min & " + sec + "s";
+     }
+  } else {
+    sec = duration % 60;
+    min = (duration - seconds)/60;
+    hr = (duration - seconds - min*60) % 3600;
+    return hr + "h & " + min + "min & " + sec + "s";
+  }
+}
 
 function countDown() {
-  duration = duration-(1/hertz);
-  var accel = Bangle.getAccel();
-  addLine(Date.now(), accel.x, accel.y, accel.z, accel.diff, accel.mag);
-  counter ++;
-  if(duration <= 0) {
-    clearInterval(counterInterval);
-    counterInterval = undefined;
-    //setWatch(startTimer, BTN2);
-    outOfTime();
-    return;
+  if(!paused) {
+    duration = duration-(1/hertz);
+    var accel = Bangle.getAccel();
+    addLine(Date.now(), accel.x, accel.y, accel.z, accel.diff, accel.mag);
+    counter ++;
+    if(duration <= 0) {
+      clearInterval(counterInterval);
+      counterInterval = undefined;
+      setWatch(startRecording, BTN2);
+      outOfTime();
+      return;
+    }
   }
 
   g.clear();
   g.setFontAlign(0,0);
-  g.setFont("Vector", 80);
+  g.setFont("Vector", 20);
   //draw current counter value
-  g.drawString(duration, g.getWidth()/2, g.getHeight()/2);
-  g.setFont("Vector", 15);
-  g.drawString(counter, g.getWidth()/2, g.getHeight()/4);
+  g.drawString("Time left :", g.getWidth()/2, g.getHeight()/4);
+  g.drawString(printDurationLeft(duration), g.getWidth()/2, g.getHeight()/2);
+  g.setFont("Vector", 20);
+  g.drawString("press BTN1 to pause", g.getWidth()/2, g.getHeight()*3/4);
   g.flip();
 }
 
-function endTimer() {
- duration = 0.0;
-}
-
-function startTimer() {
-  duration = durationInSeconds;
+function startRecording() {
+  fileName = getFileName(username, getNbAsString(fileNumber));
+  file = require("Storage").open(fileName,"a");
+  duration = durationInSeconds(hours, minutes, seconds);
+  initialDuration = duration;
+  createFirstLine();
   countDown();
+  setWatch(pauseUnpause, BTN1, repeat= true);
   if(!counterInterval) counterInterval = setInterval(countDown, 1000/hertz);
 }
 
-
-E.showMessage("Press BTN1 to start");
-setWatch(startTimer, BTN1);
-setWatch(endTimer, BTN3);
-
-
 Bangle.loadWidgets();
 Bangle.drawWidgets();
+showMenu();
